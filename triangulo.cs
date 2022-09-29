d3183948bae7cbf51acd2bfbdc6598f8b2c0690e
‘AdrianGilGar’ <adri5050@hotmail.es>
2022-09-29 19:26:34 +0200

    Sigue avancando lento

--- triangulo.cs
+++ triangulo.cs
@@ -4,6 +4,17 @@ class Triangulo
 	static void Main()
 	{
 		int ancho;
-		Console.WriteLine("Cual sera el ancho del triangulo");
-		ancho= Convert.ToInt32(Console.Read());
+		do{
+			Console.WriteLine("Cual sera el ancho del triangulo (impar)");
+			ancho= Convert.ToInt32(Console.ReadLine());
+			if (ancho % 2 == 0){
+				Console.WriteLine("Tiene que ser impar");
+			}
+			if (ancho <= 3){
+				Console.WriteLine("Venga, uno mas grande");
+			}
+		}while ((ancho % 2 == 0) || (ancho <= 3));
+		Console.WriteLine("Tr");
+	}
+}
 		
