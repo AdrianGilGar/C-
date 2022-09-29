2ccd235362e11c10130b09db6f6b398dc84c38ef
‘AdrianGilGar’ <adri5050@hotmail.es>
2022-09-29 18:56:22 +0200

    Ya no esta solo using

--- triangulo.cs
+++ triangulo.cs
@@ -1,1 +1,9 @@
-using
+using System;
+class Triangulo
+{
+	static void Main()
+	{
+		int ancho;
+		Console.WriteLine("Cual sera el ancho del triangulo");
+		ancho= Convert.ToInt32(Console.Read());
+		
