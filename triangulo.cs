5aba9bf7e16e8d84089d2572b901f85589cd64f0
‘AdrianGilGar’ <adri5050@hotmail.es>
2022-09-30 14:12:29 +0200

    Triangulo 1.0

--- triangulo.cs
+++ triangulo.cs
@@ -3,18 +3,28 @@ class Triangulo
 {
 	static void Main()
 	{
-		int ancho;
+		int ancho, esp;
 		do{
 			Console.WriteLine("Cual sera el ancho del triangulo (impar)");
 			ancho= Convert.ToInt32(Console.ReadLine());
 			if (ancho % 2 == 0){
 				Console.WriteLine("Tiene que ser impar");
 			}
-			if (ancho <= 3){
+			if ((ancho <= 3) && (ancho % 2 != 0)){
 				Console.WriteLine("Venga, uno mas grande");
 			}
 		}while ((ancho % 2 == 0) || (ancho <= 3));
-		Console.WriteLine("Tr");
+		for (int i = ancho; i >= 1; i-=2){
+			
+			for (esp = (ancho - i) / 2; esp > 0; esp--){
+				Console.Write(" ");
+			}
+			for (int ast = i; ast > 0; ast--){
+				Console.Write("*");
+			}
+			Console.WriteLine();
+		}
+			
 	}
 }
 		
